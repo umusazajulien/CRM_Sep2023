1cb9371124cda338a01d10a9fe72f19ac47ec18d
umusazajulien <122743092+umusazajulien@users.noreply.github.com>
2023-02-19 00:47:07 -0500

    cmmitted

---
+++ src/main/java/variousConcepts/GitChanges.java
@@ -0,0 +1,12 @@
+package variousConcepts;
+
+public class GitChanges {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("git test");
+
+	}
+
+}
